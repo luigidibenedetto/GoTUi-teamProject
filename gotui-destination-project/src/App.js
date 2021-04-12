1df1b737b71228d5c86a96a7e3671df4fca378e6
Luigi Di Benedetto <ing.luigidibenedetto@gmail.com>
2021-04-12 11:42:58 +0200

    feat: routing

--- gotui-destination-project/src/App.js
+++ gotui-destination-project/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './Pages/HomePage'
+import DestinationPage from './Pages/DestinationPage'
+import ActivityPage from './Pages/ActivityPage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/:lang/:destination/:activity">
+            <ActivityPage />
+          </Route>
+          <Route path="/:lang/:destination/">
+            <DestinationPage />
+          </Route>
+          <Route path="/:lang">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
