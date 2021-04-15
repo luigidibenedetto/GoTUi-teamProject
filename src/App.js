8aa8c78c2f05bdf0394363df0c70e725871f5e07
Giancarlo M. Saverino <giancasaverino@gmail.com>
2021-04-15 10:50:41 +0200

    Feat: Hero and TakeCare banner added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import HomePage from './pages/HomePage'
 import DestinationPage from './pages/DestinationPage'
 import ActivityPage from './pages/ActivityPage'
+import Hero from './Components/Hero'
+import TakeCare from './Components/TakeCare'
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,6 +21,8 @@ function App() {
     <Router>
       <div className="App">
         <h1>Pluto</h1>
+        <Hero />
+        <TakeCare />
         <Switch>
           <Route path="/:lang/:destination/:activity">
             <ActivityPage />
---
+++ src/Components/TakeCare/index.jsx
@@ -0,0 +1,29 @@
+import './index.scss';
+
+function TakeCare() {
+    return <div class='TakeCare'>
+        <a href="https://tuitakescareofyou.gotui.com/uk/">
+            <div class="banner_trusted_2mdG-">
+                <div class="banner_trusted__icon_utoGF">
+                    <div class="banner_trusted__icon_utoGF">
+                        <img src="https://tui-b2c-static.imgix.net/icons/trusted_tui.svg" alt="trusted tui" title="" loading="lazy" class="icon_2m0vQ" />
+                        </div>
+                    </div>
+                <div class="banner_trusted__title_2_J4w">
+                    TUI TAKES CARE OF YOU
+                </div>
+                <div class="banner_trusted__content_2-z8-">
+                    <div>
+                        At TUI we want you to discover your destination with peace of mind. All of our experiences take every care to keep you safe. What’s more, they come with a flexible 24-hour cancellation policy and our value guarantee.
+                        <span class="banner_trusted__content__moreinfo_205SJ">
+                            Click here to find out more
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <img id='takecare_img' alt='takecare.alt' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAAB3ElEQVQ4jY2TPWhTURTH/+e8l7QNJg0qCJpCSdEnONTFDk5udfILs9mS6iJYpI4VnURXUcRJrFG3gm3tXLpIxaUUIVA/iLRJayQ2Nk1q7cu99zjUF/JhQv7jOff/O+fcwyERAQAQEaoVjsdOirgP93L+W5svJpeq8xVfPWD/yPkepekBWbjETl8nM0h9XtkRV72xLbmdn5hJ/xdw4OaVkC6U7oBwg6MRH0d7fGTbQDoLIYKUXa2/rO5C6SdWOHh/4/HrLQCwvZZUoZi2Dh/ysdPbRZ0dNeOQCMj2WXTiaMCUtsdUKn0dQHcNAEZCVr+DViJjYAW6/NqI34txS0eVIqEwRgfONMTbApyORDE3PIbF9dX2AKPHTyESCFbMExeGMTL9EguZVHuAxY3vmBscwtkjfS3NTQELuQzi72bwaGAQ8elEUzNQtQUiLsmf3X3eCt/n1uBMPQUy2bqSDCIuNXQgjHnJ5kzTUt47rUQY8w0AG2ZcLX/bkc2t5m5mlJdTv22Y8QZAPjGbBPHl8oePRb2yruu9BjBu8msRGrF8YjZZGb3+mA5ePee4ip9xoKOfj/UGSRuYtR/b+ldxyW/paz+fv/0EtLhGT91DF2NgubtXnu4VXk1N1vzFP99f037PUFbu4yIAAAAASUVORK5CYII=" />
+    </div>
+}
+
+export default TakeCare;
