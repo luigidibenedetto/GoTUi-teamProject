2425db3848b7cb7ee6df9bede0cd7860a33b586b
Giancarlo M. Saverino <giancasaverino@gmail.com>
2021-04-15 16:59:49 +0200

    Feat: videoblock html added

---
+++ src/Components/VideoBlock/index.jsx
@@ -0,0 +1,25 @@
+import './index.scss';
+
+function VideoBlock() {
+    return <section class="home__benefits_2OH9o">
+                <section class="home__benefits__content_1vYnz">
+                    <div class="home__benefits__content__section_6wu2M">
+                        <div class="benefits_280kw">
+                            <h2 class="title__h2_1bk59">
+                            Travel inspiration
+                            </h2>
+                        <span class="benefits__subtitle_1PnBc">
+                             With TUI, the world is at your feet
+                        </span>
+                        <div class="benefits__video_ezefK">
+                            <div class="benefits__embed_sXDdH">
+                                <video controls="controls" preload="metadata" class="video_1XFBK">
+                                    <source src="https://tui-b2c-static.imgix.net/videos/TUI_Collection_Experiences+UK.mp4" type="video/mp4" />
+                                </video>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </section>
+}
