d7a4a2c81876474db4659bfb50a4c3d58bad72e9
Luigi Di Benedetto <ing.luigidibenedetto@gmail.com>
2021-04-19 16:35:36 +0200

    fix: conflict resolved
    relates to #4

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-export default function HomePage() {
-    return(
-        <>
-            <main>
-                <h1>HomePage</h1>
-            </main>
-        </>
-=======
 import DestinationCard from '../components/DestinationCard/index';
 import ActivityCard from '../components/ActivityCard/index';
 
@@ -17,6 +8,5 @@ export default function HomePage() {
             <DestinationCard />
             <ActivityCard />
         </div>
->>>>>>> cd85836f521150a0cd10dad4b832a64710ea536b
     );
 }
