50382116aa3b5747f089eadb7598005927001e7c
gionatan-lombardi <gionatan.lombardi@gmail.com>
2021-04-19 09:29:38 +0200

    fix: up in prev

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,5 @@
 import Carousel from '../components/Carousel/index';
 import TopCountries from '../components/TopCountries/index'
-import DestinationCard from '../components/DestinationCard';
-import ActivityCard from '../components/ActivityCard';
 import VideoBlock from '../components/VideoBlock';
 
 import { destinations, activities } from "../utils/static"
@@ -11,8 +9,6 @@ export default function HomePage() {
     <div className="Home">
 			<Carousel title={"TOP DESTINATIONS"} cards={destinations} selection={"destination"} />
       <Carousel title={"TOP ACTIVITIES"} cards={activities} selection={"activity"} />
-      <DestinationCard />
-      <ActivityCard />
       <VideoBlock/>
       <TopCountries />
 		</div>
