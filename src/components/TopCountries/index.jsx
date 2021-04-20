685bc034d747f9a07046cbf923baa819399e854e
Fassari Luigi <fassari.loy@gmail.com>
2021-04-20 08:29:03 +0200

    Added API to carousel

--- src/components/TopCountries/index.jsx
+++ src/components/TopCountries/index.jsx
@@ -1,24 +1,57 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios"
 import DestinationCard from '../DestinationCard/index';
 
 import './style.scss'
 
-function TopCountries({ countries, children }) {
+function TopCountries({ countries }) {
     const [active, setActive] = useState(0);
+    const [ aryCountries, setAryCountries ] = useState([]);
 
     const TopCountries = countries.map((data, index) => (
-        < span className={`countries ${index === active ? "active" : ""}`} onClick={() => { setActive(index) }}> {`${data.name}`}</ span >
+        < span className={`countries ${index === active ? "active" : ""}`} onClick={() => { setActive(index) }}> {data.name}</ span >
     ))
 
-    const topDestinations = countries.map((data, index) => (
-        <div className={`destinationsWrap ${index === active ? "toShow" : ""}`}>{(data.destinations.map((activity) =>
-            <div className="destinations">
-                <DestinationCard destination={activity} />
-            </div>
-            
-        ))}</div>)
+    let prova;
 
-    );
+    useEffect(() => {
+        prova = countries.map((country) => {
+            getTopCountries(country.id)
+            console.log("COUNTRY", country)
+        })  
+
+
+        async function getTopCountries(id) {
+            const {data : eightCountries} = await axios.get(`https://fe-tui-apiproxy.musement.com/top-countries/${id}/cities?limit=8`)
+            //const {data : aryCountries} = await axios.get(`https://fe-tui-apiproxy.musement.com/top-countries/161/cities?limit=8`)
+        
+            setAryCountries(...aryCountries, eightCountries);
+
+    }
+    
+    }, []);
+
+    setTimeout( 
+        console.log("PROVA", prova),
+        console.log("attivita spagna", aryCountries)
+    
+        , 3000);
+
+   
+
+  /*   const topDestinations = countries.map((data, index) => (
+        <div className={`destinationsWrap ${index === active ? "toShow" : ""}`}>
+            {(aryCountries.map((activity) =>
+                <div className="destinations">
+                    <DestinationCard destination={activity} />
+                </div>
+            ))}
+        </div>)
+    ); */
+    
+
+    
+
     return (
         <div className={'TopCountriesContainer'}>
             <h2> Wohin soll's gehen? </h2>
@@ -27,11 +60,26 @@ function TopCountries({ countries, children }) {
                     {TopCountries}
                 </div>
                 <div className={'TopDestinations'}>
-                    {topDestinations}
+               
                 </div>
             </div >
         </div>
     );
 }
+
+{/*
+const topDestinations = countries.map((data, index) => (
+    <div className={`destinationsWrap ${index === active ? "toShow" : ""}`}>{(data.destinations.map((activity) =>
+        <div className="destinations">
+          <DestinationCard destination={activity} />
+        </div>
+        
+    ))}</div>)
+
+);
+
+  {topDestinations} 
+{topDestinations}
+    */}
 
 export default TopCountries;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import axios from "axios"
+
 import Hero from '../../components/Hero'
 import TakeCare from '../../components/TakeCare'
 import Carousel from '../../components/Carousel/index';
@@ -13,20 +16,49 @@ import "./styles.scss"
 import { destinations, activities, countries } from "../../utils/static"
 
 export default function HomePage() {
+
+  const [ topCities, setTopCities ] = useState([]);
+  const [ topActivities, setTopActivities ] = useState([]);
+  const [ topCountries, setTopCountries ] = useState([]);
+
+  const getTopCities = async () => {
+    const {data : topCities} = await axios.get('https://fe-tui-apiproxy.musement.com/top-cities?limit=10')
+    setTopCities(topCities)
+  }
+
+  const getTopActivities = async () => {
+    const {data : topActivities} = await axios.get('https://fe-tui-apiproxy.musement.com/top-activities?sort_by=-relevance&limit=10')
+    setTopActivities(topActivities)
+  }
+
+  const getTopCountries = async () => {
+    const {data : topCountries} = await axios.get('https://fe-tui-apiproxy.musement.com/top-countries')
+    setTopCountries(topCountries)
+  }
+
+    
+
+
+  useEffect(() => {
+    getTopCities();
+    getTopActivities();
+    getTopCountries();
+  }, []);
+    
   return (
     <div className="Home">
       <Hero />
       <TakeCare />
-      <Carousel title={"TOP DESTINATIONS"} cards={destinations}>
-        {destinations.map((card) => (
+      <Carousel title={"TOP DESTINATIONS"} cards={topCities}>
+        {topCities.map((card) => (
           <div className="destination_slot">
             <DestinationCard destination={card} />
           </div>
         ))}
       </Carousel>
 
-      <Carousel title={"TOP ACTIVITIES"} cards={activities}>
-        {activities.map((card) => (
+      <Carousel title={"TOP ACTIVITIES"} cards={topActivities}>
+        {topActivities.map((card) => (
           <div className="activity_slot">
             <ActivityCard activity={card} />
           </div>
@@ -37,7 +69,7 @@ export default function HomePage() {
         <VideoBlock />
         <InfoBlock />
       </div>
-      <TopCountries countries={countries} />
+      <TopCountries countries={topCountries} />
 		</div>
   );
 }
