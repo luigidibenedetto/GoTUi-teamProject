6b388c328623afca3e01525542854d1ff68d3160
Matteo Zappalà <matteozappala@hotmail.it>
2021-04-23 12:20:13 +0200

    Feat: ActivityHero and ModalGallery created

---
+++ src/components/ActivityHero/index.jsx
@@ -0,0 +1,13 @@
+import './style.scss';
+
+function ActivityHero() {
+  return (
+    <div className="ActivityHero">
+      <div className="hero_wrapper">
+
+      </div>
+      {/* <ModalGallery /> */}
+    </div>
+  )
+}
+export default ActivityHero 
---
+++ src/components/ModalGallery/index.jsx
@@ -0,0 +1,14 @@
+import './style.scss';
+
+function ModalGallery() {
+  return (
+    <div className="ModalGallery">
+      <div className="modal_overlay">
+        <div className="modal_body">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+export default ModalGallery 
