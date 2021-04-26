d29317369332045e800a75a065a59b10ee8f7bdb
Fassari Luigi <fassari.loy@gmail.com>
2021-04-26 11:05:25 +0200

    Fix: Hero-Gallery mobile

--- src/components/ActivityHero/index.jsx
+++ src/components/ActivityHero/index.jsx
@@ -5,6 +5,9 @@ import ModalGallery from '../ModalGallery'
 import './style.scss';
 
 function ActivityHero({ contentUuid }) {
+
+  const widthPanel = window.innerWidth
+
   const [photoInModal, setPhotoInModal] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -20,6 +23,8 @@ function ActivityHero({ contentUuid }) {
 
   return (
     <div className="ActivityHero">
+
+      {widthPanel > 768 ? 
       <div className="hero_wrapper">
 
         <div className="hero_image">
@@ -67,6 +72,45 @@ function ActivityHero({ contentUuid }) {
         </section>
 
       </div>
+      : <div className="hero_wrapper">
+          <div className="hero_carousel">
+            <section className="gallery">
+              <div className="gallery_carousel">
+                *
+                <picture className="image gallery_carousel_image">
+                  <source 
+                    media="(max-width:320px)"
+                    srcSet="https://images.musement.com/cover/0037/82/thumb_3681910_cover_header.jpeg?q=50&fit=crop&auto=format&w=320&h=200 1x, https://images.musement.com/cover/0037/82/thumb_3681910_cover_header.jpeg?q=50&fit=crop&auto=format&w=640&h=400 2x"
+                  />
+                  <source 
+                    media="(max-width:430px)"
+                    srcSet="https://images.musement.com/cover/0037/82/thumb_3681910_cover_header.jpeg?q=50&fit=crop&auto=format&w=430&h=268.75 1x, https://images.musement.com/cover/0037/82/thumb_3681910_cover_header.jpeg?q=50&fit=crop&auto=format&w=860&h=537.5 2x"
+                  />
+                  <source 
+                    media="(max-width:1024px)"
+                    srcSet="https://images.musement.com/cover/0037/82/thumb_3681910_cover_header.jpeg?q=50&fit=crop&auto=format&w=1024&h=400 1x, https://images.musement.com/cover/0037/82/thumb_3681910_cover_header.jpeg?q=50&fit=crop&auto=format&w=2048&h=800 2x"
+                  />
+                  <img
+                    className="image" alt="prova"
+                    src="https://images.musement.com/cover/0037/82/thumb_3681910_cover_header.jpeg?q=50&fit=crop&auto=format&w=1024&h=400"
+                  />
+                </picture>
+              </div>
+
+              <div className="gallery_dots_box">
+                *
+                <div className="dot">
+
+                </div>
+              </div>
+
+            </section>
+          
+          </div>
+        
+
+      </div>
+      }
 
       <ModalGallery
         isOpen={modalIsOpen}
