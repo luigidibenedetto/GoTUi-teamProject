c06912fe894cf592e9aa4d4f268182bfda26206e
Giancarlo M. Saverino <giancasaverino@gmail.com>
2021-04-27 12:58:13 +0200

    Feat: show more/less buttons functions added

--- src/components/ActivityContentBody/index.jsx
+++ src/components/ActivityContentBody/index.jsx
@@ -1,6 +1,32 @@
+import { useState } from 'react';
 import './style.scss';
 
 function ActivityContentBody() {
+
+    const [descriptionVisible, setDescription] = useState(false);
+    const [infoVisible, setInfo] = useState(false);
+    
+
+    function ShowMoreLess(className, stateVariable, stateFunction) {
+        
+        const classToChange = document.querySelector(`.${className}`);
+
+        if (!stateVariable) {
+
+            classToChange.style.maxHeight = 'none';
+            classToChange.style.overflow = 'visible';
+
+            stateFunction(true);
+        }
+
+        else {
+            classToChange.style.maxHeight = '90px';
+            classToChange.style.overflow = 'hidden';
+
+        stateFunction(false);
+        }
+    }
+
 
     return (
     <div>
@@ -41,12 +67,12 @@ function ActivityContentBody() {
                 <h2 className="content__container__title">Beschreibung</h2>   <div>
                     <div>
                         <input id="description" type="checkbox" className="readMore__trigger" />
-                            <div className="readMore__text">
+                            <div className="readMore__description__text">
                                 <p>Wenn es einen Must-Do-Ausflug bei einem Urlaub auf Kos gibt, dann ist es die Fahrt zur Vulkaninsel Nisyros. Die griechische Legende besagt, dass er aus einem Stein geschaffen wurde, der von dem Gott Poseidon geworfen wurde. Schon die einstündigen Reise dorthin ist ein Traum. Wir besuchen heute Mandraki, Nikia, den Stefanos-Krater und vieles mehr. Dies ist einer der exklusiven TUI Collection Ausflüge, die Ihnen die Höhepunkte Ihres Reiseziels auf besondere Weise vermitteln – Einzigartig, Authentisch, Verantwortlich.</p>
                                 <p>Sobald wir im Hafen von Mandraki, der Hauptstadt der Insel, angedockt haben, brechen wir in das antike Dorf Nikia am Rande des Stefanos-Kraters auf. Verlieren Sie sich im Labyrinth der gepflasterten Steinspuren oder finden Sie einen Ort in der Nähe des Kraterrandes, wo Sie die Hitze spüren können – ein einzigartiges Gefühl.</p>
                                 <p>Danach geht es zurück nach Mandraki. Die zuckerbedeckten Häuser erstrecken sich über diese Amphitheater-förmige Stadt, die im Schatten einer mittelalterlichen Burg und des klippenseitigen Klosters der Panagia Spiliani liegt. Es bleibt auch Zeit, die Balkonhäuser der Stadt zu bewundern oder in einer Taverne zu Abend zu essen, bevor es zurück nach Kos geht.</p>
                             </div>
-                            <label for="description" className="readMore__btn">
+                            <label for="description" className="readMore__btn" onClick={() => ShowMoreLess('readMore__description__text', descriptionVisible, setDescription)}>
                                 <span className="readMore__btn__less">
                                     Weniger anzeigen
                                 </span>
@@ -105,12 +131,12 @@ function ActivityContentBody() {
                     <div>
                         <div>
                             <input id="info" type="checkbox" className="readMore__trigger" />
-                                <div className="readMore__text">
+                                <div className="readMore__info__text">
                                     <p>Der Krater ist recht steil, weshalb kleinen Kindern, Schwangeren und Personen mit gesundheitlichen Beschwerden von einem Besuch im Inneren abgeraten wird. Speisen und Getränke nicht im Preis inbegriffen</p>.
                                     <p>Speisen und Getränke nicht im Preis inbegriffen.</p>.
                                     <p>Am Vulkan ist eine Nachhaltigkeitsgebühr in der Höhe von 3 Euro zu entrichten</p>
                                     </div>
-                                    <label for="info" className="readMore__btn">
+                                    <label for="info" className="readMore__btn" onClick={() => ShowMoreLess("readMore__info__text", infoVisible, setInfo)}>
                                         <span className="readMore__btn__less">
                                             Weniger anzeigen
                                         </span>
