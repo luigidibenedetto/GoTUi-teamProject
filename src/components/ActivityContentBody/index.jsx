6091f1b3e7921b20b05593b9aebf59f1f7c14e09
Giancarlo M. Saverino <giancasaverino@gmail.com>
2021-04-27 11:40:13 +0200

    Feat: ActivityContentBody and ActivityContentHead mixed

--- src/components/ActivityContentBody/index.jsx
+++ src/components/ActivityContentBody/index.jsx
@@ -4,128 +4,128 @@ function ActivityContentBody() {
 
     return (
     <div>
-        <section class="content">
-            <section class="content__highlights">
-                <h2 class="content__highlights__title">Highlights</h2>
+        <section className="content">
+            <section className="content__highlights">
+                <h2 className="content__highlights__title">Highlights</h2>
                 <div>
-                    <ul class="list">
-                        <li class="list__row">
-                            <span class="content__highlights__icon">
+                    <ul className="list">
+                        <li className="list__row">
+                            <span className="content__highlights__icon">
                             </span>
-                            <span class="list__row__item">Erkunden der Vulkaninsel Nisyros
+                            <span className="list__row__item">Erkunden der Vulkaninsel Nisyros
                             </span>
                         </li>
-                        <li class="list__row">
-                            <span class="content__highlights__icon">
+                        <li className="list__row">
+                            <span className="content__highlights__icon">
                             </span>
-                            <span class="list__row__item">
+                            <span className="list__row__item">
                                 Besuch von Nikia und Mandraki
                             </span>
                         </li>
-                        <li class="list__row">
-                            <span class="content__highlights__icon"></span>
-                            <span class="list__row__item">
+                        <li className="list__row">
+                            <span className="content__highlights__icon"></span>
+                            <span className="list__row__item">
                                 Gang durch den Vulkankrater
                             </span>
                         </li>
-                        <li class="list__row">
-                            <span class="content__highlights__icon"></span>
-                            <span class="list__row__item">
+                        <li className="list__row">
+                            <span className="content__highlights__icon"></span>
+                            <span className="list__row__item">
                                 Diese Route wird ausschließlich von TUI angeboten – NUR BEI TUI
                             </span>
                         </li>
                     </ul>
                 </div>
             </section>
-            <section class="content__container">
-                <h2 class="content__container__title">Beschreibung</h2>   <div>
+            <section className="content__container">
+                <h2 className="content__container__title">Beschreibung</h2>   <div>
                     <div>
-                        <input id="description" type="checkbox" class="readMore__trigger" />
-                            <div class="readMore__text">
+                        <input id="description" type="checkbox" className="readMore__trigger" />
+                            <div className="readMore__text">
                                 <p>Wenn es einen Must-Do-Ausflug bei einem Urlaub auf Kos gibt, dann ist es die Fahrt zur Vulkaninsel Nisyros. Die griechische Legende besagt, dass er aus einem Stein geschaffen wurde, der von dem Gott Poseidon geworfen wurde. Schon die einstündigen Reise dorthin ist ein Traum. Wir besuchen heute Mandraki, Nikia, den Stefanos-Krater und vieles mehr. Dies ist einer der exklusiven TUI Collection Ausflüge, die Ihnen die Höhepunkte Ihres Reiseziels auf besondere Weise vermitteln – Einzigartig, Authentisch, Verantwortlich.</p>
                                 <p>Sobald wir im Hafen von Mandraki, der Hauptstadt der Insel, angedockt haben, brechen wir in das antike Dorf Nikia am Rande des Stefanos-Kraters auf. Verlieren Sie sich im Labyrinth der gepflasterten Steinspuren oder finden Sie einen Ort in der Nähe des Kraterrandes, wo Sie die Hitze spüren können – ein einzigartiges Gefühl.</p>
                                 <p>Danach geht es zurück nach Mandraki. Die zuckerbedeckten Häuser erstrecken sich über diese Amphitheater-förmige Stadt, die im Schatten einer mittelalterlichen Burg und des klippenseitigen Klosters der Panagia Spiliani liegt. Es bleibt auch Zeit, die Balkonhäuser der Stadt zu bewundern oder in einer Taverne zu Abend zu essen, bevor es zurück nach Kos geht.</p>
                             </div>
-                            <label for="description" class="readMore__btn">
-                                <span class="readMore__btn__less">
+                            <label for="description" className="readMore__btn">
+                                <span className="readMore__btn__less">
                                     Weniger anzeigen
                                 </span>
-                                <span class="readMore__btn__more">
+                                <span className="readMore__btn__more">
                                     Mehr erfahren
                                 </span>
                             </label>
                         </div>
                     </div>
                 </section>
-                <section class="content__fluid">
-                    <section class="content__fluid__container">
-                        <h2 class="content__fluid__container__title">Inbegriffen</h2>
+                <section className="content__fluid">
+                    <section className="content__fluid__container">
+                        <h2 className="content__fluid__container__title">Inbegriffen</h2>
                         <div>
-                            <ul class="list">
-                                <li class="list__row">
-                                    <div class="content__fluid__container__icon">
-                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" class="icon_2m0vQ" />
+                            <ul className="list">
+                                <li className="list__row">
+                                    <div className="content__fluid__container__icon">
+                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" className="icon" />
                                     </div>
-                                    <span class="list__row__item">Transfer</span>
+                                    <span className="list__row__item">Transfer</span>
                                 </li>
-                                <li class="list__row_1CCXe">
-                                    <div class="content__fluid__container__icon">
-                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" class="icon" />
+                                <li className="list__row">
+                                    <div className="content__fluid__container__icon">
+                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" className="icon" />
                                     </div>
-                                    <span class="list__row__item">Transfer</span>
+                                    <span className="list__row__item">Transfer</span>
                                 </li>
-                                <li class="list__row">
-                                    <div class="content__fluid__container__icon">
-                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" class="icon" />
+                                <li className="list__row">
+                                    <div className="content__fluid__container__icon">
+                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" className="icon" />
 
                                     </div>
-                                    <span class="list__row__item">Guide</span>
+                                    <span className="list__row__item">Guide</span>
                                 </li>
-                                <li class="list__row">
-                                    <div class="content__fluid__container__icon">
-                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" class="icon" />
+                                <li className="list__row">
+                                    <div className="content__fluid__container__icon">
+                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" className="icon" />
                                     </div>
-                                    <span class="list__row__item">Bootstour</span>
+                                    <span className="list__row__item">Bootstour</span>
                                 </li>
-                                <li class="list__row">
-                                    <div class="content__fluid__container__icon">
-                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" class="icon" />
+                                <li className="list__row">
+                                    <div className="content__fluid__container__icon">
+                                        <img src="https://tui-b2c-static.imgix.net/icons/flag.svg" alt="included flag" title="" loading="lazy" className="icon" />
                                     </div>
-                                    <span class="list__row__item">Audioguide</span>
+                                    <span className="list__row__item">Audioguide</span>
                                 </li>
                             </ul>
                         </div>
                     </section>
                 </section>
-                <section class="content__fluid"></section> 
-                <section class="content__container">
-                    <h2 class="title__h2_1bk59 cotent__container__title_2mPge">
+                <section className="content__fluid"></section> 
+                <section className="content__container">
+                    <h2 className="content__container__title">
                         Wichtig zu wissen
                     </h2>
                     <div>
                         <div>
-                            <input id="info" type="checkbox" class="readMore__trigger" />
-                                <div class="readMore__text">
+                            <input id="info" type="checkbox" className="readMore__trigger" />
+                                <div className="readMore__text">
                                     <p>Der Krater ist recht steil, weshalb kleinen Kindern, Schwangeren und Personen mit gesundheitlichen Beschwerden von einem Besuch im Inneren abgeraten wird. Speisen und Getränke nicht im Preis inbegriffen</p>.
                                     <p>Speisen und Getränke nicht im Preis inbegriffen.</p>.
                                     <p>Am Vulkan ist eine Nachhaltigkeitsgebühr in der Höhe von 3 Euro zu entrichten</p>
                                     </div>
-                                    <label for="info" class="readMore__btn">
-                                        <span class="readMore__btn__less">
+                                    <label for="info" className="readMore__btn">
+                                        <span className="readMore__btn__less">
                                             Weniger anzeigen
                                         </span>
-                                        <span class="readMore__btn__more">
+                                        <span className="readMore__btn__more">
                                              Mehr erfahren
                                         </span>
                                     </label>
                                 </div>
                             </div>
                         </section>
-                        <section class="content__fluid">
-                            <section class="content__fluid__container">
+                        <section className="content__fluid">
+                            <section className="content__fluid__container">
                                 <div>
                                     <div>
-                                        <h2 class="refundPolicies__title">
+                                        <h2 className="refundPolicies__title">
                                             Stornierungsbedingungen
                                         </h2>
                                         <span>Sie erhalten eine Erstattung in Höhe von 100%, wenn Sie bis zu 1 Tag vor Ihrem Erlebnis stornieren.
@@ -133,16 +133,16 @@ function ActivityContentBody() {
                                     </div>
                                 </div>
                             </section>
-                            <section class="content__fluid__container">
-                                <h2 class="content__fluid__container__title">
+                            <section className="content__fluid__container">
+                                <h2 className="content__fluid__container__title">
                                     Anbieter
                                 </h2>
                             <div>TUI HELLAS AE</div>
                             </section>
                         </section>
-                        <section class="content__fluid"></section>
-                        <section class="content__container">
-                            <h2 class="content__container__title">
+                        <section className="content__fluid"></section>
+                        <section className="content__container">
+                            <h2 className="content__container__title">
                                 Buchungsgebühr
                             </h2>
                             <span>Für diese Buchung fallen keine zusätzlichen Kosten an.</span>
---
+++ src/pages/ActivityPage/index.jsx
@@ -0,0 +1,25 @@
+import ActivityHero from '../../components/ActivityHero'
+import ActivityContentHead from "../../components/ActivityContentHead";
+import ActivityRelated from '../../components/ActivityRelated';
+import ActivityContentBody from '../../components/ActivityContentBody';
+
+import './style.scss';
+
+export default function ActivityPage() {
+
+	//deve arrivare come props
+  const uuid = "47543853-76bc-4cbb-b4e1-4c2030f216f9"
+
+  return (
+    <div>
+			<ActivityHero contentUuid={uuid} />
+			<div className='ActivityContent'>
+				<div className='ActivityContent__container'>
+				<ActivityContentHead />
+				<ActivityContentBody />
+				</div>
+			</div>
+      <ActivityRelated/>
+		</div>
+	);
+}
