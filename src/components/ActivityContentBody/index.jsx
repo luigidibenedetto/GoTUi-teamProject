f2c779ca4680013907959769427292df655c6647
Giancarlo M. Saverino <giancasaverino@gmail.com>
2021-04-28 15:32:07 +0200

    Fix: show more/less buttons logic remade in ActivityContentBody

--- src/components/ActivityContentBody/index.jsx
+++ src/components/ActivityContentBody/index.jsx
@@ -25,30 +25,10 @@ function ActivityContentBody( { uuid }) {
 
     console.log('activitiesTAX= ', activitiesTax);
 
-    function ShowMoreLess(className, stateVariable, stateFunction, infordescription) {
+    function ShowMoreLess(stateVariable, stateFunction) {
+
+        stateVariable ? stateFunction(false) : stateFunction(true);
         
-        const classToChange = document.querySelector(`.${className}`);
-        const btnMore = document.querySelector(`.readMore__${infordescription}__btn__more`);
-        const btnLess = document.querySelector(`.readMore__${infordescription}__btn__less`);
-
-        if (!stateVariable) {
-
-            classToChange.style.maxHeight = 'none';
-            classToChange.style.overflow = 'visible';
-            btnMore.style.display = 'none';
-            btnLess.style.display = 'inline';
-
-            stateFunction(true);
-        }
-
-        else {
-            classToChange.style.maxHeight = '90px';
-            classToChange.style.overflow = 'hidden';
-            btnMore.style.display = 'inline';
-            btnLess.style.display = 'none';
-
-        stateFunction(false);
-        }
     }
 
 
@@ -93,16 +73,13 @@ function ActivityContentBody( { uuid }) {
                         <input id="description" type="checkbox" className="readMore__trigger" />
                             <div className="readMore__description__text">
                                 <p>Wenn es einen Must-Do-Ausflug bei einem Urlaub auf Kos gibt, dann ist es die Fahrt zur Vulkaninsel Nisyros. Die griechische Legende besagt, dass er aus einem Stein geschaffen wurde, der von dem Gott Poseidon geworfen wurde. Schon die einstündigen Reise dorthin ist ein Traum. Wir besuchen heute Mandraki, Nikia, den Stefanos-Krater und vieles mehr. Dies ist einer der exklusiven TUI Collection Ausflüge, die Ihnen die Höhepunkte Ihres Reiseziels auf besondere Weise vermitteln – Einzigartig, Authentisch, Verantwortlich.</p>
-                                <p>Sobald wir im Hafen von Mandraki, der Hauptstadt der Insel, angedockt haben, brechen wir in das antike Dorf Nikia am Rande des Stefanos-Kraters auf. Verlieren Sie sich im Labyrinth der gepflasterten Steinspuren oder finden Sie einen Ort in der Nähe des Kraterrandes, wo Sie die Hitze spüren können – ein einzigartiges Gefühl.</p>
-                                <p>Danach geht es zurück nach Mandraki. Die zuckerbedeckten Häuser erstrecken sich über diese Amphitheater-förmige Stadt, die im Schatten einer mittelalterlichen Burg und des klippenseitigen Klosters der Panagia Spiliani liegt. Es bleibt auch Zeit, die Balkonhäuser der Stadt zu bewundern oder in einer Taverne zu Abend zu essen, bevor es zurück nach Kos geht.</p>
+                                <div className={descriptionVisible ? 'visible':'invisible'}>
+                                    <p>Sobald wir im Hafen von Mandraki, der Hauptstadt der Insel, angedockt haben, brechen wir in das antike Dorf Nikia am Rande des Stefanos-Kraters auf. Verlieren Sie sich im Labyrinth der gepflasterten Steinspuren oder finden Sie einen Ort in der Nähe des Kraterrandes, wo Sie die Hitze spüren können – ein einzigartiges Gefühl.</p>
+                                    <p>Danach geht es zurück nach Mandraki. Die zuckerbedeckten Häuser erstrecken sich über diese Amphitheater-förmige Stadt, die im Schatten einer mittelalterlichen Burg und des klippenseitigen Klosters der Panagia Spiliani liegt. Es bleibt auch Zeit, die Balkonhäuser der Stadt zu bewundern oder in einer Taverne zu Abend zu essen, bevor es zurück nach Kos geht.</p>
+                                </div>
                             </div>
-                            <label htmlFor="description" className="readMore__btn" onClick={() => ShowMoreLess('readMore__description__text', descriptionVisible, setDescription, 'description')}>
-                                <span className="readMore__description__btn__less">
-                                    Weniger anzeigen
-                                </span>
-                                <span className="readMore__description__btn__more">
-                                    Mehr erfahren
-                                </span>
+                            <label htmlFor="description" className="readMore__description__btn" onClick={() => ShowMoreLess(descriptionVisible, setDescription)}>
+                                {descriptionVisible ? 'Weniger anzeigen' : 'Mehr erfahren'}
                             </label>
                         </div>
                     </div>
@@ -156,19 +133,15 @@ function ActivityContentBody( { uuid }) {
                         <div>
                             <input id="info" type="checkbox" className="readMore__trigger" />
                                 <div className="readMore__info__text">
-                                    <p>Der Krater ist recht steil, weshalb kleinen Kindern, Schwangeren und Personen mit gesundheitlichen Beschwerden von einem Besuch im Inneren abgeraten wird. Speisen und Getränke nicht im Preis inbegriffen.</p>.
-                                    <div className='showParagraph' style={{display: infoVisible ? '':'none'}}>
-                                    <p>Speisen und Getränke nicht im Preis inbegriffen.</p>.
+                                    <p>Der Krater ist recht steil, weshalb kleinen Kindern, Schwangeren und Personen mit gesundheitlichen Beschwerden von einem Besuch im Inneren abgeraten wird. Speisen und Getränke nicht im Preis inbegriffen.</p>
+                                    <br/>
+                                    <div className={infoVisible ? 'visible':'invisible'}>
+                                    <p>Speisen und Getränke nicht im Preis inbegriffen.</p>
+                                    <br/>
                                     <p>Am Vulkan ist eine Nachhaltigkeitsgebühr in der Höhe von 3 Euro zu entrichten</p>
                                     </div>
                                     </div>
-                                    <label htmlFor="info" className="readMore__btn" onClick={() => ShowMoreLess("readMore__info__text", infoVisible, setInfo, 'info')}>
-                                        <span className="readMore__info__btn__less">
-                                            Weniger anzeigen
-                                        </span>
-                                        <span className="readMore__info__btn__more">
-                                             Mehr erfahren
-                                        </span>
+                                    <label htmlFor="info" className="readMore__info__btn" onClick={() => ShowMoreLess(infoVisible, setInfo)}> {infoVisible ? 'Weniger anzeigen' : 'Mehr erfahren'}
                                     </label>
                                 </div>
                             </div>
